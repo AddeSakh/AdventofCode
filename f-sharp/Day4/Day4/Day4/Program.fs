071e164dbefc45ed6725975ed97472604b0c8c2f
adde.sakh <adde.sakh@gmail.com>
2016-06-07 00:30:32 +0200

    WIP

--- f-sharp/Day4/Day4/Day4/Program.fs
+++ f-sharp/Day4/Day4/Day4/Program.fs
@@ -1,7 +1,7 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open System.Security.Cryptography
+
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    let md5 = MD5.Create()
+    let input = "yzbqklnj"
