5061ea98dec4648d8bcda1227ecefe6400e99472
adde.sakh <adde.sakh@gmail.com>
2016-06-17 23:50:48 +0200

    WIP solution 4

--- f-sharp/Day4/Day4/Day4/Program.fs
+++ f-sharp/Day4/Day4/Day4/Program.fs
@@ -1,7 +1,11 @@
-﻿open System.Security.Cryptography
+﻿let showAnswer a b = printfn "Answer for part 1: %A, for part 2: %A" a b
 
-
-[<EntryPoint>]
-let main argv = 
-    let md5 = MD5.Create()
-    let input = "yzbqklnj"
+let run() = 
+  let input = "yzbqklnj"
+  let solve startSeq = 
+    let hasher = Security.Cryptography.MD5.Create()
+    Seq.initInfinite id
+    |> Seq.find(fun i -> 
+      input + i.ToString() 
+      |> Text.Encoding.ASCII.GetBytes 
+      |> hasher.ComputeHash
