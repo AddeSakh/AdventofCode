a81b76b2c8fd4b4fe99a7f6a017f9fb86496a967
adde.sakh <adde.sakh@gmail.com>
2016-06-19 14:34:30 +0200

    Solution 4, take some time to calculate as its bruteforce.

--- f-sharp/Day4/Day4/Day4/Program.fs
+++ f-sharp/Day4/Day4/Day4/Program.fs
@@ -1,5 +1,7 @@
 ﻿let showAnswer a b = printfn "Answer for part 1: %A, for part 2: %A" a b
 
+open System
+
 let run() = 
   let input = "yzbqklnj"
   let solve startSeq = 
@@ -9,3 +11,9 @@ let run() =
       input + i.ToString() 
       |> Text.Encoding.ASCII.GetBytes 
       |> hasher.ComputeHash
+      |> Array.map(fun e -> e.ToString("X2"))
+      |> String.concat ""
+      |> fun s -> s.StartsWith startSeq)
+  (solve "00000", solve "000000") ||> (showAnswer)
+
+run()
