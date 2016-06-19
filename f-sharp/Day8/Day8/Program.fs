90bafe68f8ab728dcabc457e945b8a4c13aa30db
adde.sakh <adde.sakh@gmail.com>
2016-06-19 23:38:22 +0200

    Solution for day 8.

---
+++ f-sharp/Day8/Day8/Program.fs
@@ -0,0 +1,16 @@
+﻿let showAnswer a b = printfn "Answer for part №1: %A, for part №2: %A" a b
+
+let run() = 
+  let lines = System.IO.File.ReadAllLines("c:\\temp\\day8.txt") 
+  let escapedLength = lines |> Array.sumBy String.length
+  let unescape s = 
+    let us = System.Text.RegularExpressions.Regex.Unescape(s)
+    us.[1 .. us.Length - 2]
+  let unescapedLength = lines |> Array.sumBy (unescape >> String.length)
+  let escape s = 
+    let es = System.Text.RegularExpressions.Regex.Escape(s).Replace("\"", "\\\"")
+    "\"" + es + "\""
+  let escapedtwiceLength = lines |> Array.sumBy (escape >> String.length)
+  (escapedLength - unescapedLength, escapedtwiceLength - escapedLength) ||> showAnswer
+
+run()
