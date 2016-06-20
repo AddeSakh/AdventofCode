a41ef1b705ca734ff570daa370b4060d448a05d1
adde.sakh <adde.sakh@gmail.com>
2016-06-20 22:43:27 +0200

    Day10. WIP.

---
+++ f-sharp/Day10/Day10/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+open System.IO
+
+let getInput() = "3113322113"
+
+let iterate n func arg = 
+  let rec go i v =  
+    match i with
+    | 0 -> v
+    | _ -> go <| i - 1 <| func v
+  go n arg
+
+let showAnswer a b = printfn "Answer for part 1: %A, for part 2: %A" a b
+
+let run() =
+  let input, parseSeq = getInput(), Seq.map (string >> Int32.Parse) >> Seq.toList
+  let lookAndSay sq =
+
+  (solve 40, solve 50) ||> showAnswer
+
+run()
